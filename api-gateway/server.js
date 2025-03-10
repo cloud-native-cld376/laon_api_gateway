4917459f336f17eb9e54abde8b2be045c5e5a551
kheang <bunkheangheng99@gmail.com>
2025-03-10 13:29:46 +0700

    modifed api gateways

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -8,13 +8,10 @@ app.use(cors());
 app.use(express.json());
 
 // Configuration for service endpoints
-const LOGIN_SERVICE = process.env.LOGIN_SERVICE || 'http://23.20.134.178:3004';
 const LOAN_SERVICE = process.env.LOAN_SERVICE || 'http://34.201.33.221:3002';
 const ADMIN_SERVICE = process.env.ADMIN_SERVICE || 'http://23.20.134.178:3003';
 const PAYMENT_SERVICE = process.env.PAYMENT_SERVICE || 'http://34.201.33.221:3005';
 
-// Auth routes
-app.use('/auth', proxy(LOGIN_SERVICE));
 // Route to different services
 app.use('/loan', proxy(LOAN_SERVICE));
 app.use('/admin', proxy(ADMIN_SERVICE));
