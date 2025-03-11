024e36002c56bd27aa2bebf88f80f2f45323bf40
kheang <bunkheangheng99@gmail.com>
2025-03-11 11:41:06 +0700

    modifed server.js

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const proxy = require('express-http-proxy');
+require('dotenv').config();
 
 const app = express();
 
@@ -8,9 +9,9 @@ app.use(cors());
 app.use(express.json());
 
 // Configuration for service endpoints
-const LOAN_SERVICE = process.env.LOAN_SERVICE || 'http://34.201.33.221:3002';
-const ADMIN_SERVICE = process.env.ADMIN_SERVICE || 'http://23.20.134.178:3003';
-const PAYMENT_SERVICE = process.env.PAYMENT_SERVICE || 'http://34.201.33.221:3005';
+const LOAN_SERVICE = process.env.LOAN_SERVICE;
+const ADMIN_SERVICE = process.env.ADMIN_SERVICE;
+const PAYMENT_SERVICE = process.env.PAYMENT_SERVICE;
 
 // Route to different services
 app.use('/loan', proxy(LOAN_SERVICE));
@@ -20,4 +21,7 @@ app.use('/payment', proxy(PAYMENT_SERVICE));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`API Gateway is running on port ${PORT}`);
+    console.log(`LOAN_SERVICE: ${LOAN_SERVICE}`);
+    console.log(`ADMIN_SERVICE: ${ADMIN_SERVICE}`);
+    console.log(`PAYMENT_SERVICE: ${PAYMENT_SERVICE}`);
 }); 
